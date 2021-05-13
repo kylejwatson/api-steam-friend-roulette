886f2e3e38f1efc9f87c888f71257119ef956593
Kyle Watson <mail@watsonk.me>
2021-05-13 22:00:44 +0200

    Added many more endpoints, for getting game details and shared games

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import https from 'https';
+import http from 'http';
 import cors from 'cors';
 import { config } from 'dotenv';
 config()
@@ -23,10 +23,18 @@ interface SummaryResponse {
         players: [];
     }
 }
+interface Game {
+    appid: number;
+}
+interface GameResponse {
+    response?: {
+        games: Game[];
+    }
+}
 
 const getPromise = (url: string) => {
     return new Promise((resolve, reject) => {
-        https.get(url, (response) => {
+        http.get(url, (response) => {
             let data = '';
             response.on('data', (chunk) => {
                 data += chunk;
@@ -44,18 +52,27 @@ const getPromise = (url: string) => {
 }
 
 
-const steamUrl = 'https://api.steampowered.com/ISteamUser';
+const steamFriendUrl = 'http://api.steampowered.com/ISteamUser';
+const steamPlayerUrl = 'http://api.steampowered.com/IPlayerService'
+const steamAppUrl = 'http://store.steampowered.com/api/appdetails';
 
 const makeFriendsUrl = (steamId: string) => {
-    return `${steamUrl}/GetFriendList/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${steamId}`;
+    return `${steamFriendUrl}/GetFriendList/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${steamId}`;
 }
 const makeSummaryUrl = (steamIds: string) => {
-    return `${steamUrl}/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API_KEY}&steamids=${steamIds}`;
+    return `${steamFriendUrl}/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API_KEY}&steamids=${steamIds}`;
+}
+const makeOwnedUrl = (steamId: string) => {
+    return `${steamPlayerUrl}/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${steamId}&include_appinfo=true`;
+}
+const makeAppUrl = (appIds: string) => {
+    return `${steamAppUrl}/?key=${process.env.STEAM_API_KEY}&appids=${appIds}`;
 }
 
 
 const getFriends = (steamId: string) => getPromise(makeFriendsUrl(steamId));
 const getSummary = (steamIds: string) => getPromise(makeSummaryUrl(steamIds));
+const getOwned = (steamId: string) => getPromise(makeOwnedUrl(steamId));
 
 app.get('/friends', async (req, res) => {
     const steamId = req.query["steamid"] as string;
@@ -64,10 +81,11 @@ app.get('/friends', async (req, res) => {
     }
     try {
         const friendsResponse: FriendResponse = await getFriends(steamId);
-        if (!friendsResponse.friendslist?.friends) {
+        const friends = friendsResponse.friendslist?.friends;
+        if (!friends) {
             res.sendStatus(404);
         } else {
-            res.send(friendsResponse.friendslist.friends);
+            res.send(friends);
         }
     } catch (error) {
         res.status(500).send(error);
@@ -81,10 +99,11 @@ app.get('/summary', async (req, res) => {
     }
     try {
         const summaryResponse: SummaryResponse = await getSummary(steamIds);
-        if (!summaryResponse.response?.players) {
+        const players = summaryResponse.response?.players;
+        if (!players) {
             res.sendStatus(404);
         } else {
-            res.send(summaryResponse.response.players);
+            res.send(players);
         }
     } catch (error) {
         res.status(500).send(error);
@@ -98,16 +117,85 @@ app.get('/friendSummary', async (req, res) => {
     }
     try {
         const friendsResponse: FriendResponse = await getFriends(steamId);
-        if (!friendsResponse.friendslist?.friends) {
+        const friends = friendsResponse.friendslist?.friends;
+        if (!friends) {
             return res.sendStatus(404);
         }
-        const steamIds = friendsResponse.friendslist.friends.map(friend => friend.steamid);
+        const steamIds = friends.map(friend => friend.steamid);
         const summaryResponse: SummaryResponse = await getSummary(steamIds.join());
-        if (!summaryResponse.response?.players) {
+        const players = summaryResponse.response?.players;
+        if (!players) {
             res.sendStatus(404);
         } else {
-            res.send(summaryResponse.response.players);
+            res.send(players);
         }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.get('/owned', async (req, res) => {
+    const steamId = req.query["steamid"] as string;
+    if (!steamId) {
+        return res.status(400).send('Bad Request: Please provide steamid in the query')
+    }
+    try {
+        const ownedResponse: GameResponse = await getOwned(steamId);
+        const games = ownedResponse.response?.games;
+        if (!games) {
+            res.sendStatus(404);
+        } else {
+            res.send(games);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+const filterShared = (ownedLists: Game[][]) => {
+    // console.log(ownedLists[0]);
+    return ownedLists[0].filter(game => ownedLists.every(list => list.find(searchGame => searchGame.appid === game.appid)));
+}
+
+app.get('/shared', async (req, res) => {
+    const steamIds = req.query["steamids"] as string;
+    if (!steamIds) {
+        return res.status(400).send('Bad Request: Please provide steamids in the query')
+    }
+    try {
+        const ids = steamIds.split(',');
+        const ownedResponse: GameResponse[] = await Promise.all(ids.map(id => {
+            return getOwned(id);
+        }));
+        const games = ownedResponse.map(owned => owned.response?.games);
+        if (games.every(game => !game)) {
+            res.sendStatus(404);
+        } else {
+            const shared = filterShared(games);
+            console.log(shared);
+            res.send(shared);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+const getApps = (appIds: string) => getPromise(makeAppUrl(appIds));
+
+app.get('/app', async (req, res) => {
+    const appIds = req.query["appids"] as string;
+    if (!appIds) {
+        return res.status(400).send('Bad Request: Please provide appids in the query')
+    }
+    try {
+        const appResponse: any = await getApps(appIds);
+        console.log(appResponse)
+        res.send(appResponse)
+        // if (!summaryResponse.response?.players) {
+        //     res.sendStatus(404);
+        // } else {
+        //     res.send(summaryResponse.response.players);
+        // }
     } catch (error) {
         res.status(500).send(error);
     }
